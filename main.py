a3c50afb722698350662447f84b861fd393d2e60
qq1416071480 <52318761+qq1416071480@users.noreply.github.com>
2021-08-30 18:20:53 +0800

    Update main.py
    123

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pytest
 import os
 
 if __name__ == '__main__':
-    pytest.main(['-sq', '--alluredir'])
+    pytest.main(['-sq'])
     # 运行结果目录  报告存放目录
     # os.system(r"allure generate allure-results -o allure-report --clean")
     # os.system('allure open allure-report')
